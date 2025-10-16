ca804e53d8543026fc44974c2e912af175c20206
Anton Barinoff <antonbarinoff1@gmail.com>
2025-10-17 00:26:22 +0700

    fix script_3.sh

--- script_3.sh
+++ script_3.sh
@@ -1,5 +1,3 @@
-read -p "Введите директорию для архивации" 
-date=$(date +%Y%m%d)
-archive_name="archive_${date}.tar.gz"
-tar -czf "$archive_name" "$path"
-echo "Архив создан: $archive_name"
+#!/bin/bash
+read -p "Укажите директорию: " dir
+tar -czvf $(date +"%Y-%m-%d").tar.gz $dir
